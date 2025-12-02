342498c7e5c58c0ca6c59f088bff7e654208f9f5
louisemogensenn <eaa23lmog@students.eaaa.dk>
2025-12-02 16:39:09 +0100

    Oprettelse af rammen til Layout.jsx samt denne

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,14 +1,21 @@
-import { useLocation } from "react-router-dom"
+import LogoSektion from "./components/LogoSektion";
+import Soegefelt from "./components/Soegefelt";
+import Navigation from "./components/Navigation";
+import { Outlet } from "react-router-dom";
 
 export default function Layout() {
-    const nuvLokation = useLocation(); // Returnerer en liste af informationer om den nuværende lokation
-    const sti = nuvLokation.pathname; // Tilgå én specifik information fra ovenstående - nem lige stien, der returnerer "/" eller "/om" eller "/kontakt" osv.
 
     return (
         <>
-        {/* Heri angives rammen for indholdet - altså det, der ikke ændrer sig, der i stedet danner rammen for indholdet */}
+        {/* Heri angives rammen for indholdet - altså det, der ikke ændrer sig */}
+        <LogoSektion />
+        <Soegefelt />
+        <Navigation/>
+        <main> {/* Henviser til main.jsx */}
+            <Outlet /> {/* Her indsættes det indhold, der ændrer sig */}
+        </main>
 
-        
+
         </>
     )
 }
