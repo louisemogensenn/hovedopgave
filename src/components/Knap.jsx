ba49e0d858f242ebcfceac5dfd648bc2ba8f759d
louisemogensenn <eaa23lmog@students.eaaa.dk>
2025-12-03 13:54:35 +0100

    Opsætning af knap

--- src/components/Knap.jsx
+++ src/components/Knap.jsx
@@ -2,10 +2,19 @@ import mystyle from "./Knap.module.css";
 
 export default function Knap( {knaptekst} ) {
 
+    const tekstIKnap = knaptekst.trim().toLowerCase(); // Fjern mellemrum og gør teksten små bogstaver
+
+    let previewClass = mystyle.preview;
+
+    if ({knaptekst}.toLowerCase === "download") {
+        previewClass += "" + mystyle.Aktiv;
+    }
+
     return (
         <>
             <section>
                 <button className={mystyle.knap}>{knaptekst}</button>
+                <p className={mystyle.preview}>Preview</p>
             </section>
         </>
     )
