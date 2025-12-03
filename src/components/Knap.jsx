725417eecb9eed86ab3ec56f97fea62b28cbfe79
louisemogensenn <eaa23lmog@students.eaaa.dk>
2025-12-03 13:43:19 +0100

    Opdatering af søgefelt og oprettelse af knap

--- src/components/Knap.jsx
+++ src/components/Knap.jsx
@@ -2,6 +2,14 @@ import mystyle from "./Knap.module.css";
 
 export default function Knap( {knaptekst} ) {
 
+    return (
+        <>
+            <section>
+                <button className={mystyle.knap}>{knaptekst}</button>
+            </section>
+        </>
+    )
+
     
 }
 
