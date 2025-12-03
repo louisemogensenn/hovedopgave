b755b54f4b3dc8971f000e8605e8a77d378c0812
louisemogensenn <eaa23lmog@students.eaaa.dk>
2025-12-03 10:35:22 +0100

    Tilføjelse af link til bundlogo

--- src/components/BundLogo.jsx
+++ src/components/BundLogo.jsx
@@ -1,13 +1,15 @@
 import mystyle from "./BundLogo.module.css";
 import bundLogoSvg from "../assets/dotlegalLogoMedPayOff.svg";
+import { Link } from "react-router-dom";
 
 export default function BundLogo() {
   return (
-  <>
-  <section className={mystyle.bundLogo}>
-    <img src={bundLogoSvg} alt=".legal logo - Compliance Software" />
-  </section>
-  </>
-
+    <>
+      <section className={mystyle.bundLogo}>
+        <Link to="https://www.dotlegal.com/en/">
+          <img src={bundLogoSvg} alt=".legal logo - Compliance Software" />
+        </Link>
+      </section>
+    </>
   );
 }
