a7570ffcb88f7db776d00afaf3492221f0352b68
louisemogensenn <eaa23lmog@students.eaaa.dk>
2025-12-03 10:34:15 +0100

    Tilføjelse af link til både kundelogo og .legal-logo i LogoSektion.jsx

--- src/components/LogoSektion.jsx
+++ src/components/LogoSektion.jsx
@@ -1,19 +1,22 @@
 import kundeLogo from '../assets/dmjxLogo.png';
 import mystyle from './LogoSektion.module.css';
 import dotLegalLogo from '../assets/dotLegalLogo.svg';
+import { Link } from 'react-router-dom';
 
 export default function LogoSektion(){
     return (
         <>
         <section className={mystyle.logoSektion}>
-        <img className={mystyle.kundeLogo} src={kundeLogo} alt="DMJX - Danmarks Medie- og Journalisthøjskole"/>
-        <article className={mystyle.trustCenterSektion}>
-            <p className={mystyle.trustCenterTekst}>TRUST CENTER</p>
-            <figure className={mystyle.poweredBySektion}>
-                <p>powered by</p>
-                <img src={dotLegalLogo} alt=".legal-logo"/>
-            </figure>
-        </article>
+            <Link to="https://www.dmjx.dk"><img className={mystyle.kundeLogo} src={kundeLogo} alt="DMJX - Danmarks Medie- og Journalisthøjskole"/></Link>
+            <Link to="https://www.dotlegal.com/en/">
+                <article className={mystyle.trustCenterSektion}>
+                    <p className={mystyle.trustCenterTekst}>TRUST CENTER</p>
+                    <figure className={mystyle.poweredBySektion}>
+                        <p>powered by</p>
+                        <img src={dotLegalLogo} alt=".legal-logo"/>
+                    </figure>
+                </article>
+            </Link>
         </section>
         </>
     )
